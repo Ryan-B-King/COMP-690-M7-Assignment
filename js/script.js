01b08e23b99e2cb53547daf35c0eb8d36b75ae8d
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-03-21 21:34:45 -0700

    update

--- js/script.js
+++ js/script.js
@@ -4,11 +4,18 @@
 
 
 // ADD EMPLOYEE
+let form;
 form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
+    e.preventDefault()
 
     // GET THE VALUES FROM THE TEXT BOXES
-
+    const $ = (id) => document.getElementById(id)
+    console.log('ID: ' + $('id').value);
+    console.log('Name: ' + $('name').value);
+    console.log('Extension ' + $('ext').value);
+    console.log('Email: ' + $('email').value);
+    console.log('Department: ' + $('department').value);
     // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
 
     // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
